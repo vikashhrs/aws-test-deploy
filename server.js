301afb89a7777cad3883a0fa2bda3cfc29776904
Vikash Kumar Sharma <vikashhrs@gmail.com>
2018-05-05 01:11:27 +0530

    lets test

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const morgan = require('morgan')
+
+const app = express();
+
+app.get('/',(req,res) => {
+	console.log(req.connection.remoteAddress);
+	res.status(200);
+	res.send("<h1>Welcome</h1>");
+})
+
+
+app.listen(3000,() => {
+	console.log("Server running at port 3000");
+});
